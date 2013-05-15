6e6a9ac311bba568fd64e400e8d24fbeeeb9a827
ilulu <lucifer.li@outlook.com>
2013-05-16 03:31:58 +0800

    完成二维码扫描集成

--- TeatApp/src/com/example/teatapp/MainGroupActivity.java
+++ TeatApp/src/com/example/teatapp/MainGroupActivity.java
@@ -1,12 +1,23 @@
 package com.example.teatapp;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.ActivityGroup;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -17,6 +28,7 @@ public class MainGroupActivity extends ActivityGroup {
 	private ImageView btnModuleHome = null;
 	private ImageView contactModuleHome = null;
 	private ImageView fileModuleHome = null;
+	private ImageView btnScanQR=null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,20 @@ public class MainGroupActivity extends ActivityGroup {
 		custTitle = (TextView) findViewById(R.id.cust_title);
 		btnModuleHome = (ImageView) findViewById(R.id.btnModule1);
 
+		btnScanQR=(ImageView)findViewById(R.id.btnScanQR);
+		
+		btnScanQR.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(
+						"com.google.zxing.client.android.SCAN");
+				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+				startActivityForResult(intent, 0);
+			}
+		});
+		
 		container.addView(getLocalActivityManager().startActivity(
 				"ModuleHome",
 				new Intent(MainGroupActivity.this, HomeActivity.class)
@@ -93,6 +119,43 @@ public class MainGroupActivity extends ActivityGroup {
 
 	}
 
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				
+				HttpClient httpClient = new DefaultHttpClient();
+				String uri = getResources().getString(R.string.server_uri);
+				uri+="loginFormScan.do";
+				
+				StringBuffer address = new StringBuffer(uri);
+				address.append("?id=" + contents);
+				HttpPost httpPost = new HttpPost(address.toString());
+				try {
+					HttpResponse httpResponse = httpClient.execute(httpPost);
+					if (httpResponse.getStatusLine().getStatusCode() == 200) {
+						String result = EntityUtils.toString(httpResponse
+								.getEntity());
+					}
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				// Handle successful scan
+				Log.i("aa", " 条形码为:" + contents + " 条码类型为: " + format);// 利用页面的textveiw显示扫描后的结果
+
+			} else if (resultCode == RESULT_CANCELED) {
+				// Handle cancel
+				Log.i("aa", " 扫描失败!");
+			}
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
